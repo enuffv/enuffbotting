793b14c8e494b28074f57832e2b244b6d07bdc64
cant pronounce uor's :( <61086941+enuffv@users.noreply.github.com>
2023-07-29 00:48:15 +0100

    Updated new Selectors
    Added selectors that will be used for new versions, as well as improving them.

--- modules/constant/selector/selectorindex.js
+++ modules/constant/selector/selectorindex.js
@@ -5,6 +5,7 @@ module.exports = {
     subscribe: "#subscribe-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill",
     videosCount: "#videos-count > span:nth-child(1)",
     
+    videoDuration: "#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span:nth-child(2) > span.ytp-time-duration",
     videoTitleinSearch : "#video-title",
     shortsTitleinSearch : "#details > a",
     catchErrorInComment : "#message > span",
@@ -12,4 +13,5 @@ module.exports = {
     submitComment: "#submit-button > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill",
     videosTab: "#tabsContent > tp-yt-paper-tab:nth-child(5)",
     videoSelection: "#contents",
+    watchvideo: "#movie_player > div.ytp-cued-thumbnail-overlay > button",
 }
