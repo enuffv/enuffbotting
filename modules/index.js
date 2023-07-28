22cfa936de5b43673d8bc6e38c1426ca8c915d23
cant pronounce uor's :( <61086941+enuffv@users.noreply.github.com>
2023-07-29 00:45:24 +0100

    Removed outdated Features
    Outdated features. As the project gets more support, I'll add them and keep them maintained for long term support.

--- modules/index.js
+++ modules/index.js
@@ -2,10 +2,7 @@
 module.exports = {
     chromepath : require("./src/chromepathfinder"),
     subsribe : require("./src/subscribeChannel"),
-    copycommnet : require("./src/copyComment"),
-    autoscroll : require("./src/autoScroll"),
     manualComment : require("./src/manualComment"),
-    closePage : require("./src/closePage"),
     likeVideos : require("./src/likeVideos"),
     randomUserAgent : require("./src/userAgent"),
     Logger : require("./src/Logger"),
