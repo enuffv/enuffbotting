d21eee9459c58302864e8b084ace3e86a9ea99b7
cant pronounce uor's :( <61086941+enuffv@users.noreply.github.com>
2023-07-29 00:50:34 +0100

    Small Updates
    Minor updates added to improve cookie parsing logic.

--- cookiemaker/main.js
+++ cookiemaker/main.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const directory = __dirname; // Use the current directory where the code is located
+const directory = __dirname;
 
-// Function to filter files with "cookie" in the filename
 function filterCookiesFiles(file) {
   return file.includes('cookie');
 }
-
-// Function to read contents from each file
 function readFiles(files) {
   const cookiesList = [];
   
@@ -20,34 +17,27 @@ function readFiles(files) {
     cookiesList.push(cookies);
     
     if (index !== files.length - 1) {
-      cookiesList.push(); // Add a null value to separate cookie sets
+      cookiesList.push();
     }
   });
   
   return cookiesList;
 }
 
-// Function to write contents to cookieslist.json
 function writeToFile(contents) {
   const jsonData = JSON.stringify(contents, null, 2);
   const updatedJsonData = jsonData.replace(/\[\],/g, '],');
   fs.writeFileSync('cookieslist.json', updatedJsonData);
 }
 
-// Get a list of files in the directory
 fs.readdir(directory, (err, files) => {
   if (err) {
     console.error('Error reading directory:', err);
     return;
   }
-  
-  // Filter files with "cookie" in the filename
   const cookiesFiles = files.filter(filterCookiesFiles);
-  
-  // Read contents from each file
   const contents = readFiles(cookiesFiles);
   
-  // Write contents to cookieslist.json
   writeToFile(contents);
   
   console.log('Cookies list created successfully!');
